2c06d36410b62915dfede6cef45d88d015c9c4d5
Pushkar <pushkar723@gmail.com>
2019-01-20 14:02:55 +0530

    SeleniumTestNGMavenproject

---
+++ src/firsttestngpackage/FirstTestNGfile.java
@@ -0,0 +1,27 @@
+package firsttestngpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTestNGfile {
+	 public String baseUrl = "http://demo.guru99.com/test/newtours/";
+	    String driverPath = "C:\\Users\\Pushkar\\Videos\\Setup files\\chromedriver.exe";
+	    public WebDriver driver ; 
+	    
+	     
+	  @Test
+	  public void verifyHomepageTitle() {
+	       
+	      System.out.println("launching Chrome browser"); 
+	      System.setProperty("webdriver.chrome.driver", driverPath);
+	      driver = new ChromeDriver();
+	      driver.get(baseUrl);
+	      String expectedTitle = "Welcome: Mercury Tours";
+	      String actualTitle = driver.getTitle();
+	      Assert.assertEquals(actualTitle, expectedTitle);
+	      driver.close();
+	  }
+	
+}
